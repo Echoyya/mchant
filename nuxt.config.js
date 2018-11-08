b82dcda009a51719416c4d71eebe63aed45dc189
naheya <nahy@startimes.com.cn>
2018-11-08 11:59:44 +0800

    接口调试

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,8 @@ module.exports = {
         retry: { retries: 0 }
     },
     proxy: {
-        '/payment/mc/': env.ms_host
+        '/payment/mc/': env.ms_host,
+        '/cms/vup/': env.ms_host
     },
 
     /*
@@ -65,16 +66,16 @@ module.exports = {
         /*
     ** You can extend webpack config here
     */
-        // extend(config, ctx) {
-        //     // Run ESLint on save
-        //     if (ctx.isDev && ctx.isClient) {
-        //         config.module.rules.push({
-        //             enforce: 'pre',
-        //             test: /\.(js|vue)$/,
-        //             loader: 'eslint-loader',
-        //             exclude: /(node_modules)/
-        //         })
-        //     }
-        // }
+        extend(config, ctx) {
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
 }
